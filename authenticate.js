8449099674f4912576ab8ec6845e081df3b6a763
Changhyun Jeong <chjeong@e1z2r3p20.42.us.org>
2019-04-27 20:09:31 -0700

    HTTPS

--- authenticate.js
+++ authenticate.js
@@ -7,11 +7,11 @@ var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 var config = require('./config.js');
 
-
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//Creating jwt token.
 exports.getToken = function(user) {
     return jwt.sign(user, config.secretKey,
         {expiresIn: 3600});
@@ -37,4 +37,40 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
         });
     }));
 
+exports.adminPassport = passport.use(new JwtStrategy(opts,
+    (jwt_payload, done) => {
+        console.log("JWT payload: ", jwt_payload);
+        User.findOne({_id: jwt_payload._id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            else if (user && user.admin) {
+                return done(null, user);
+            }
+            else {
+                return done(null, false);
+            }
+        });
+    }));
+
+
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = function(req, res, next) {
+    User.findOne({_id: req.user._id})
+    .then((user) => {
+        console.log("User: ", req.user);
+        if (user.admin) {
+            next();
+        }
+        else {
+            err = new Error('You are not authorized to perform this operation!');
+            err.status = 403;
+            return next(err);
+        } 
+    }, (err) => next(err))
+    .catch((err) => next(err))
+}
+
+
+
--- models/dishes.js
+++ models/dishes.js
@@ -15,12 +15,15 @@ var commentSchema = new Schema({
         type: String,
         required: true
     },
-    author:  {
-        type: String,
-        required: true
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+        //usePushEach : true
     }
-}, {
+},
+{
     timestamps: true
+    
 });
 
 var dishSchema = new Schema({
