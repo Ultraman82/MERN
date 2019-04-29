727695faa4421ad83168573a77b80c891429b675
Changhyun Jeong <chjeong@e1z2r3p24.42.us.org>
2019-04-28 20:49:22 -0700

    Passport Facebook

--- routes/cors.js
+++ routes/cors.js
@@ -2,17 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist =['http://localhost:3000', 'http://localhost:3443'];
+const whitelist = ['http://localhost:3000', 'https://localhost:3443'];
 var corsOptionsDelegate = (req, callback) => {
-    var cosrOptions;
-
+    var corsOptions;
+    console.log(req.header('Origin'));
     if(whitelist.indexOf(req.header('Origin')) !== -1) {
-        cosrOptions = { origin: true };
+        corsOptions = { origin: true };
     }
-    else{
-        cosrOptions = { origin:false }
+    else {
+        corsOptions = { origin: false };
     }
-    callback(null, cosrOptions);
+    callback(null, corsOptions);
 };
 
 exports.cors = cors();
